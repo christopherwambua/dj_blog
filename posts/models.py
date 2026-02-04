aa0ee4db0b765b1492fe1ae31786175c3d597962
chris <mutisyachristopher4034@gmail.com>
2026-02-04 14:49:10 +0200

    third commit

--- posts/models.py
+++ posts/models.py
@@ -9,3 +9,4 @@ class Posts(models.Model):
 
     def __str__(self):
         return self.title
+    
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
+from .models import Posts   
 
 # Create your views here.
+
+def posts(request):
+    post= Posts.objects.all()
+    context={'post':post}
+    return render(request,'post_list.html',context) 
